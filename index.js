ffe243a965ebaefe7e0f5b0e22ff459e15f9a8d6
TPL777 <tl606878@gmail.com>
2023-11-30 03:22:56 -0300

    Filtrando as tarefas ativas

--- index.js
+++ index.js
@@ -20,9 +20,27 @@ app.use(express.json())
 app.post('/completar', (requisicao, resposta) => {
     const id = requisicao.body.id 
 
-    const sql =`
+    const sql = `
         UPDATE tarefas
         SET completa = '1'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
+app.post('/descompletar', (requisicao, resposta) =>{
+    const id = requisicao.body.id
+
+    const sql = `
+        UPDATE tarefas
+        SET completa = '0'
         WHERE id = ${id}
     `
 
@@ -69,7 +87,13 @@ app.get('/', (requisicao, resposta) => {
             }
         })
 
-        resposta.render('home', {tarefas})
+        const tarefasAtivas = tarefas.filter((tarefa) =>{
+            return tarefa.completa === false && tarefa
+        })
+
+        const quantidadeTarefasAtivas = tarefasAtivas.length
+
+        resposta.render('home', {tarefas, quantidadeTarefasAtivas})
     })
 })
 
